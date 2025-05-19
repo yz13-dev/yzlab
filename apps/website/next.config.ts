ecec70c2e2b779b735de90d6c0c3700364e87e8e
yz13-dev <yztheceo@yandex.ru>
2025-05-19 21:56:05 +0500

    add website app, add typescript package

---
+++ apps/website/next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  transpilePackages: [],
+  compress: true,
+  experimental: {
+    inlineCss: true,
+    reactCompiler: true,
+    optimizeCss: true,
+    optimizeServerReact: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
