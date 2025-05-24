723c1a77b43de25d401c99542300feb2af2942ae
yz13-dev <yztheceo@yandex.ru>
2025-05-24 20:07:01 +0500

    add links extractor, update crawler endpoint

--- apps/api/app/api/[...route]/crawl/endpoint.ts
+++ apps/api/app/api/[...route]/crawl/endpoint.ts
@@ -1,4 +1,5 @@
 import { extractContent } from "@/lib/extract-content";
+import { extractLinks } from "@/lib/extract-links";
 import { fetchPageContent } from "@/lib/fetch/page";
 import { Hono } from "hono";
 
@@ -12,32 +13,70 @@ crawl.post("/", async (c) => {
 
   if (!url) return c.json({ error: "Missing URL" }, 400);
 
+  const crawlStart = performance.now();
+
   try {
     const baseUrl = new URL(url);
+
+    const pathname = baseUrl.pathname;
+
+    const isRoot = pathname === "/";
 
     const html = await fetchPageContent(url, renderJS);
     const doc = extractContent(html, url);
 
     const title = doc.title;
     const description = doc.description;
-    const tags = doc.tags;
+    const tags = isRoot ? doc.tags : [];
     const snippets = doc.snippets;
+
+    const links = extractLinks(html);
+
+    const favicon = tags.find(
+      (tag) =>
+        tag.attributes.rel === "icon" ||
+        tag.attributes.rel === "shortcut icon" ||
+        tag.attributes.rel === "icon shortcut",
+    )?.attributes?.href;
+
+    const linksWithBaseUrl = links.map((link) =>
+      new URL(link, baseUrl).toString(),
+    );
 
     console.group("[CRAWLED]");
     console.log("URL:", baseUrl.origin);
     console.log("PATH:", baseUrl.pathname);
+    console.log("LINKS:", links.length);
+    console.log("SNIPPETS:", snippets.length);
+
+    const crawledAt = new Date().toISOString();
+
+    const crawlEnd = performance.now();
+    const crawlDuration = crawlEnd - crawlStart;
+
+    console.log("DURATION:", crawlDuration, "ms");
+
     console.groupEnd();
 
     return c.json({
-      baseUrl: baseUrl.origin,
-      success: true,
+      error: null,
+      duration: crawlDuration,
+      favicon: favicon ?? null,
+      crawled_at: crawledAt,
+      domain: baseUrl.origin,
+      pathname,
       title,
       description,
       tags,
       snippets,
+      links: linksWithBaseUrl,
     });
   } catch (e) {
     console.error("[ERROR]", e);
-    return c.json({ error: "Failed to crawl" }, 500);
+    const crawlEnd = performance.now();
+    const crawlDuration = crawlEnd - crawlStart;
+
+    console.log("DURATION:", crawlDuration, "ms");
+    return c.json({ error: "Failed to crawl", duration: crawlDuration }, 500);
   }
 });
