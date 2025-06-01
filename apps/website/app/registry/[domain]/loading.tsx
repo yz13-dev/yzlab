15c7d9c3f9661572a39c1efa186b5b96b64a0631
yz13-dev <yztheceo@yandex.ru>
2025-06-01 10:18:18 +0500

    update domain registry page

--- apps/website/app/registry/[domain]/loading.tsx
+++ apps/website/app/registry/[domain]/loading.tsx
@@ -1,4 +1,3 @@
-import { Logo } from "@/components/logo";
 import Link from "next/link";
 import { Skeleton } from "ui/components/skeleton";
 
--- apps/website/app/registry/[domain]/page.tsx
+++ apps/website/app/registry/[domain]/page.tsx
@@ -1,9 +1,12 @@
-import { Logo } from "@/components/logo";
-import { ExternalLink, GlobeIcon } from "lucide-react";
+import ServerCodeBlock from "@/app/search/server-code-block";
+import { SnippetCard } from "@/app/search/snippet-card";
+import { format, parseISO } from "date-fns";
+import { GlobeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { getIndexRegisrtyItem } from "rest-api/domains";
+import { Badge } from "ui/components/badge";
 import {
   Table,
   TableBody,
@@ -12,9 +15,7 @@ import {
   TableHeader,
   TableRow,
 } from "ui/components/table";
-import { format, parseISO } from "date-fns";
-import { Badge } from "ui/components/badge";
-import { Tabs, TabsList, TabsTrigger } from "ui/components/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "ui/components/tabs";
 
 type PageProps = {
   params: Promise<{
@@ -69,62 +70,80 @@ export default async function page({ params }: PageProps) {
           </div>
         </div>
       </main>
-      <div className="max-w-4xl w-full mx-auto px-6 mb-6">
-        <Tabs defaultValue="links">
-          <TabsList>
-            <TabsTrigger value="links">Ссылки</TabsTrigger>
-            <TabsTrigger value="snippets">Снипеты</TabsTrigger>
-          </TabsList>
-        </Tabs>
-      </div>
-      <div className="max-w-4xl w-full mx-auto px-6">
-        <div className="rounded-2xl bg-card border over">
-          <Table>
-            <TableHeader>
-              <TableRow className="w-full *:w-1/4">
-                <TableHead className="pl-4">Путь</TableHead>
-                <TableHead className="md:table-cell hidden">Статус</TableHead>
-                <TableHead className="md:table-cell hidden">
-                  Последнее обновление
-                </TableHead>
-                <TableHead className="w-9" />
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {links.map((domain) => {
-                const domainName = domain.domain;
-                const isIndexed = domain.last_crawled_at !== null;
-                const lastUpdated = domain.last_crawled_at
-                  ? parseISO(domain.last_crawled_at)
-                  : null;
-                const pathname = domain.pathname;
-                return (
-                  <TableRow
-                    key={domainName + pathname}
-                    className="[&>td]:first:pl-4 [&>td]:last:pr-4"
-                  >
-                    <TableCell>
-                      <Badge variant="secondary">{pathname}</Badge>
-                    </TableCell>
-                    <TableCell className="md:table-cell hidden">
-                      <span className="text-sm">
-                        {isIndexed ? "Индексирован" : "Не индексирован"}
-                      </span>
-                    </TableCell>
-                    <TableCell className="md:table-cell hidden">
-                      <span className="text-sm">
-                        {lastUpdated
-                          ? format(lastUpdated, "dd.MM.yyyy")
-                          : "Не обновлено"}
-                      </span>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </div>
-      </div>
+      <Tabs defaultValue="links" className="max-w-4xl w-full mx-auto px-6">
+        <TabsList className="mb-6">
+          <TabsTrigger value="links">Ссылки</TabsTrigger>
+          <TabsTrigger value="snippets">Снипеты</TabsTrigger>
+        </TabsList>
+        <TabsContent value="links">
+          <div className="rounded-2xl bg-card border">
+            <Table>
+              <TableHeader>
+                <TableRow className="w-full *:w-1/4">
+                  <TableHead className="pl-4">Путь</TableHead>
+                  <TableHead className="md:table-cell hidden">Статус</TableHead>
+                  <TableHead className="md:table-cell hidden">
+                    Последнее обновление
+                  </TableHead>
+                  <TableHead className="w-9" />
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {links.map((domain) => {
+                  const domainName = domain.domain;
+                  const isIndexed = domain.last_crawled_at !== null;
+                  const lastUpdated = domain.last_crawled_at
+                    ? parseISO(domain.last_crawled_at)
+                    : null;
+                  const pathname = domain.pathname;
+                  return (
+                    <TableRow
+                      key={domainName + pathname}
+                      className="[&>td]:first:pl-4 [&>td]:last:pr-4"
+                    >
+                      <TableCell>
+                        <Badge variant="secondary">{pathname}</Badge>
+                      </TableCell>
+                      <TableCell className="md:table-cell hidden">
+                        <span className="text-sm">
+                          {isIndexed ? "Индексирован" : "Не индексирован"}
+                        </span>
+                      </TableCell>
+                      <TableCell className="md:table-cell hidden">
+                        <span className="text-sm">
+                          {lastUpdated
+                            ? format(lastUpdated, "dd.MM.yyyy")
+                            : "Не обновлено"}
+                        </span>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
+        </TabsContent>
+        <TabsContent value="snippets" className="space-y-6">
+          {snippets.map((snippet) => {
+            const lang = snippet.language;
+            const code = snippet.code;
+            const isCropped = code.length >= 200;
+            const croppedCode = isCropped
+              ? `${code.slice(0, 200)}...`
+              : code;
+            return (
+              <SnippetCard key={snippet.id} snippet={snippet}>
+                <ServerCodeBlock
+                  className="w-full bg-card relative font-mono border rounded-b-lg text-sm p-4"
+                  lang={lang}
+                >
+                  {croppedCode}
+                </ServerCodeBlock>
+              </SnippetCard>
+            );
+          })}
+        </TabsContent>
+      </Tabs>
       <footer className="max-w-4xl w-full mx-auto px-6 py-12">
         <span className="text-sm text-muted-foreground">YZ13 2025</span>
       </footer>
